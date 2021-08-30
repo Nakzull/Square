12519428a20708602ee52415d782a235e442340a
Nakzull <andr25f4@zbc.dk>
2021-08-30 10:49:40 +0200

    Add project files.

---
+++ Square/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("side = ");
+            int side = Convert.ToInt32(Console.ReadLine());
+            Square square = new Square(side);
+            Console.WriteLine("Perimeter = " + square.Perimeter(side) + "\nArea = " + square.Area(side));
+        }
+    }
+}
---
+++ Square/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Square
+{
+    class Square
+    {
+        private int side;
+
+        public int Side
+        {
+            get { return this.side; }
+            set { this.side = value; }
+        }
+        public Square(int Side)
+        {
+            side = Side;
+        }
+
+        public int Perimeter(int side)
+        {
+            return side * 4;
+        }
+        public int Area(int side)
+        {
+            return side * side;
+        }
+    }
+}
